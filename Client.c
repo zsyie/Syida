353d6154d2d0293d39eda711951d9655c7e23c6b
zsyie <48537915+zsyie@users.noreply.github.com>
2019-03-20 15:30:01 +0800

    Update Client.c

--- Client.c
+++ Client.c
@@ -28,13 +28,13 @@ int main(int argc, char const *argv[])
     // Convert IPv4 and IPv6 addresses from text to binary form 
     if(inet_pton(AF_INET, "192.168.153.133", &serv_addr.sin_addr)<=0)  
     { 
-        printf("\Invaliad address/ Address not supported \n"); 
+        printf("\invaliad address/ Address not supported \n"); 
         return -1; 
     } 
    
     if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) 
     { 
-        printf("\Connection unsuccessful \n"); 
+        printf("\connection unsuccessful \n"); 
         return -1; 
     } 
     send(sock , hello , strlen(hello) , 0 ); 
